86f382f40f0542bc1658069ef094393abc0add57
Hari Prasath <hari@haris-macbook-air.local>
2017-10-18 22:10:17 +0530

    UI test case

--- SimpleUIControlUITests/SimpleUIControlUITests.m
+++ SimpleUIControlUITests/SimpleUIControlUITests.m
@@ -35,6 +35,15 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElement *usernameTextField = app.textFields[@"UserName"];
+    [usernameTextField tap];
+    [usernameTextField typeText:@"Hari"];
+    XCUIElement *passwordSecureTextField = app.secureTextFields[@"Password"];
+    [passwordSecureTextField tap];
+    [passwordSecureTextField typeText:@"Pras"];
+    app.buttons[@"login"].tap;
 }
 
 @end
